917aff8c4335bafc975c0899b87171882eef0048
zx-in-offi <ziyasheikh2005@gmail.com>
2025-01-07 12:10:03 +0530

    Exp-11

--- src/Home.js
+++ src/Home.js
@@ -7,26 +7,19 @@
 // export default Home;
 
 
-//Experiment-10
+// Experiment-11
 import {  useState  } from "react"
+import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs,setBlogs] = useState([
-        {title:"My Blog1", body:"Body of Blog1", author:"Ramu", id:1},
-        {title:"My Blog2", body:"Body of Blog2", author:"Raju", id:2},
-        {title:"My Blog3", body:"Body of Blog3", author:"Tomm", id:3},
+        {title:"My Blog1", body:"Body of Blog1", author:"Raju", id:1},
+        {title:"My Blog2", body:"Body of Blog2", author:"Ramu", id:2},
+        {title:"My Blog3", body:"Body of Blog3", author:"Ranu", id:3}
     ]);
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id} >
-                    <h2> {blog.title} </h2>
-                    <p>Written by: {blog.author} </p>
-                </div>
-                
-            ))
-
-            }
+            <BlogList blogs={blogs} title="All Blogs!" />
         </div>
     );
 }
